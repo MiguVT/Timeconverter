845c28996fa246de05a1f3f577b86d4bd35fcf72
MiguVT <miguyt1@gmail.com>
2025-11-04 22:42:51 +0100

    feat: Remove Vercel Analytics module from Nuxt configuration, not official way

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   ssr: false,
 
-  modules: ['@vueuse/nuxt', '@nuxtjs/color-mode', '@vercel/analytics/nuxt'],
+  modules: ['@vueuse/nuxt', '@nuxtjs/color-mode'],
 
   colorMode: {
     classSuffix: '',
